833b9e336d5ea058d78af4bef33101d081d3a4da
Ludovic Potvin <2032796@etu.cegepjonquiere.ca>
2022-02-01 18:52:00 -0500

    Formulaire progress version 0.02

--- index.php
+++ index.php
@@ -8,9 +8,33 @@
     </head>
 
     <body>
-        <div id="header">
+        <header>
             <h1>Ajouter un membre</h1>
-        </div>
-    </body>
+        </header>
 
+        <div id="Core">
+            <!--FORMULAIRE-->
+            <form name="Calendrier" method="post" action="https://dicjlinux01.cegepjonquiere.ca/formdebug/debug.php">
+                <h1>Entrez les informations</h1>
+                <div id="Formulaire">
+                
+                </div>
+                <!--BOUTON ENVOYER/RESET-->
+                        <input  id="btnEnvoyer" value="Envoyer" type="submit">
+                        <input  id="btnReintialiser" value="Réinitialiser" type="reset" />
+                </div>
+            </form>
+        </div>
+
+        <div id="footer">
+            <p>Ludovic Potvin</p>
+            <p>Version 0.02</p>
+            <p>2032796</p>
+        </footer>
+    </body>
+    
+
+
+    
+    
 </html>
